32d1bfb050882b1773f727c6ef8dc4a13507e795
HongrunX <HongrunX@users.noreply.github.com>
2020-02-03 19:52:15 -0800

    assignment week4

---
+++ src/ArticleList/ArticleListItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ArticleListItem = (props) =>{
+    return(
+        <div>
+            <section>
+    <h3>{props.article.title}</h3>
+    <p>{props.article.shortText}</p>
+    <time datetime={props.article.pubYear}>{props.article.pubDate}</time>
+    <button onClick={()=> alert(`${props.article.slug}`)}>
+        show article slug
+    </button>
+            </section>
+        </div>
+    );
+}
+export default ArticleListItem;
