aeb11501e0752be448bc3ff3f612588c29ee6705
HongrunX <HongrunX@users.noreply.github.com>
2020-03-09 20:38:51 -0700

    [week-8]

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -5,12 +5,22 @@ import ArticleImage from "./ArticleImage";
 const ArticleListItem = props => {
   return (
     <div className={styles.container}>
-      <div className={styles.img}><ArticleImage article={props.article}/></div>
+      <div className={styles.img}>
+        <ArticleImage article={props.article} />
+      </div>
       <div className={styles.other}>
-      <h3 className={styles.title}>{props.article.title}</h3>
-      <p>{props.article.shortText}</p>
-      <time className={styles.pubDate} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
-      <div className={styles.button}><SlugButton slug={props.article.slug} buttonText={props.article.author}/></div>      </div>
+        <h3 className={styles.title}>{props.article.title}</h3>
+        <p>{props.article.shortText}</p>
+        <time className={styles.pubDate} dateTime={props.article.pubYear}>
+          {props.article.pubDate}
+        </time>
+      </div>
+      <div className={styles.button}>
+          <SlugButton
+            slug={props.article.slug}
+            buttonText={props.article.author}
+          />
+        </div>{" "}
     </div>
   );
 };
